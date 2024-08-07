7d9f6892b569728a31cd4a4535e8625cb4c8b5e3
soomanbaek <qortnaks311@gmail.com>
2024-08-07 23:44:58 +0900

    test

--- index.js
+++ index.js
@@ -1,48 +1,92 @@
 const axios = require('axios');
 
-const NOTION_API_URL = "https://api.notion.com/v1/databases/db4ffb466e4f4889b0ee5d309ec1c3fe/query";
+const NOTION_API_DATABASE_URL = "https://api.notion.com/v1/databases/db4ffb466e4f4889b0ee5d309ec1c3fe/query";
 const NOTION_API_KEY = "secret_xVKFAspLpkbWkQ7Tct2JmVNQl01mYCMqFv0gzqF2pl2";
+const NOTION_API_PAGE_URL = "https://api.notion.com/v1/pages/";
 
-async function updateCheckbox(userName, newFileString) {
+async function run(userName, fileString) {
   try {
-    const headers = getHeaders();
+    const pages = await getPages();
 
-    const response = await axios.post(NOTION_API_URL, {}, {headers}); // 데이터베이스에서 페이지를 조회
-    const pages = response.data.results;
-
-    newFiles = newFileString.split(',');
-    newFiles.pop();
+    filePaths = extractFilePathsFromString(fileString);
 
     for (const filePath of newFiles) {
-      const fileNameSplit = filePath.split('/');
-      console.log(fileNameSplit);
-      const fileName = fileNameSplit[fileNameSplit.length - 1];
+      const problemName = getProblemName(filePath);
+      const targetPage = getTargetPage(pages, problemName);
 
-      const problemName = fileName.split('.')[0];
-      const targetPage = pages.find(page => page.properties.이름.title[0].plain_text === problemName);
-
-      if (targetPage) {
-        const pageId = targetPage.id;
-
-        // 체크박스 업데이트
-        const updateUrl = `https://api.notion.com/v1/pages/${pageId}`;
-        const updateData = {
-          "properties": {
-            [userName]: {
-              "checkbox": true
-            }
-          }
-        };
-
-        await axios.patch(updateUrl, updateData, {headers});
-        console.log(`"${problemName}"의 체크박스를 '체크'했습니다.`);
-      } else {
+      if(isEmptyTargetPage(targetPage)){
         console.log(`"${problemName}"라는 이름을 가진 페이지를 찾을 수 없습니다.`);
+        continue;
       }
+
+      await updateCheckbox(targetPage, userName);
+
+      console.log(`"${problemName}"의 체크박스를 '체크'했습니다.`);
     }
   } catch (error) {
     console.error('오류 발생:', error.response ? error.response.data : error.message);
   }
+}
+
+async function updateCheckbox(targetPage, userName) {
+  const pageId = targetPage.id;
+
+  // 체크박스 업데이트
+  const updateUrl = getUpdateUrl(pageId);
+  const updateData = getUpdateData(userName);
+  const headers = getHeaders();
+
+  await axios.patch(updateUrl, updateData, {headers});
+}
+
+function extractFilePathsFromString(fileString) {
+  const filePaths =  fileString.split(',');
+  filePaths.pop();
+
+  return filePaths;
+}
+
+function extractFileNameFromPath(filePath) {
+  const fileNameSplit = filePath.split('/');
+  const fileName = fileNameSplit[fileNameSplit.length - 1];
+
+  return fileName;
+}
+
+function getTargetPage(pages, problemName) {
+  return pages.find(page => page.properties.이름.title[0].plain_text === problemName);
+}
+
+function isEmptyTargetPage(targetPage) {
+  return targetPage === undefined;
+}
+
+function getUpdateUrl(pageId) {
+  return NOTION_API_PAGE_URL + pageId;
+}
+
+function getUpdateData(userName) {
+  return {
+    "properties": {
+      [userName]: {
+        "checkbox": true
+      }
+    }
+  };
+}
+
+async function getPages() {
+  const response = await axios.post(NOTION_API_DATABASE_URL, {}, {headers});
+  const pages = response.data.results;
+
+  return pages;
+}
+
+function getProblemName(filePath) {
+  const fileName = extractFileNameFromPath(filePath);
+  const problemName = fileName.split('.')[0];
+
+  return problemName;
 }
 
 function getHeaders() {
@@ -53,9 +97,7 @@ function getHeaders() {
   };
 }
 
-
-
 const USER_NAME = process.env.USER_NAME;
 const NEW_FILES = process.env.NEW_FILES;
 
-updateCheckbox(USER_NAME, NEW_FILES);
+run(USER_NAME, NEW_FILES);
